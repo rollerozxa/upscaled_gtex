c18b7e234c1492aaed8412c9628879726de794fa
ROllerozxa <rollerozxa@voxelmanip.se>
2023-04-26 21:48:32 +0200

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,29 @@
+
+local scale = minetest.settings:get("upscaled_gtex_scale") or 4
+
+minetest.register_on_mods_loaded(function()
+	for id,def in pairs(minetest.registered_nodes) do
+		local tiles = def.tiles
+		local target_tiles = {}
+
+		if tiles and legal_drawtype(def.drawtype) then
+			for index,_ in pairs(tiles) do
+				if type(tiles[index]) == 'table' then
+					tiles[index].align_style = 'world'
+					tiles[index].scale = scale
+					target_tiles[index] = tiles[index]
+				else
+					target_tiles[index] = {
+						name = tiles[index],
+						align_style = 'world',
+						scale = scale
+					}
+				end
+			end
+
+			minetest.override_item(id, {
+				tiles = target_tiles
+			})
+		end
+	end
+end)
